00fdd1b2444ffdb17353b993a6545dac1c26ce22
SCatella <samuel.catella@gmail.com>
2026-07-30 14:27:07 -0600

    feat!(card): accept props from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { Component } from 'react';
 import Card from './Card.jsx';
 import './App.css';
 import 'tachyons';
+import { robots } from './assets/robots.js'
 
 class App extends Component {
   render() {
     return (
       <>
-        <Card />
+        <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
       </>
     )
   }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({ id, name, email }) => {
   return (
-    <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-      <img alt='profile-photo' src='' />
+    <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+      <img alt='profile-photo' src={`https://robohash.org/${id}?200x200`} />
       <div>
-        <h2>Name</h2>
-        <p>Email</p>
+        <h2>{name}</h2>
+        <p>{email}</p>
       </div>
     </div>
   )
