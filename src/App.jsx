eca96043fba06294d91edae9476cf9d884d2b051
SCatella <samuel.catella@gmail.com>
2026-07-30 14:13:36 -0600

    feat(card): add tachyons, format card

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Component } from 'react';
 import Card from './Card.jsx';
 import './App.css';
+import 'tachyons';
 
 class App extends Component {
   render() {
     return (
       <>
+        <Card />
       </>
     )
   }
--- src/Card.jsx
+++ src/Card.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Card = () => {
   return (
-    <>
-      <img alt='profile-photo'src='' />
+    <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+      <img alt='profile-photo' src='' />
       <div>
         <h2>Name</h2>
         <p>Email</p>
       </div>
-    </>
+    </div>
   )
 }
 
