602e571e9e5d4512aa80d954a23bd8f3506b72ae
SCatella <samuel.catella@gmail.com>
2026-07-30 21:59:55 -0600

    feat(search_box): add state, connect searchfield to state

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,32 @@ import SearchBox from './SearchBox.jsx'
 import 'tachyons';
 import { robots } from './assets/robots.js';
 
+
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: robots,
+      searchField: '',
+    }
+  }
+
+
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value })
+    const filteredRobots = this.state.robots.filter(robots => {
+      return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+    })
+    console.log(filteredRobots);
+  }
+
+
   render() {
     return (
       <div className='tc'>
         <h1>Robofriends</h1>
-        <SearchBox />
-        <CardList robots={robots}/>
+        <SearchBox searchChange={ this.onSearchChange } />
+        <CardList robots={ this.state.robots }/>
       </div>
     )
   }
