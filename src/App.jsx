77fa301466ad6c0fac9f92aee3dd0e2c1fa6c07c
SCatella <samuel.catella@gmail.com>
2026-07-30 22:03:37 -0600

    fix(search_box): move filteredrobots to inside app render function. pass filteredrobots to cardlist

--- src/App.jsx
+++ src/App.jsx
@@ -18,19 +18,19 @@ class App extends Component {
 
   onSearchChange = (event) => {
     this.setState({ searchField: event.target.value })
-    const filteredRobots = this.state.robots.filter(robots => {
-      return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
-    })
-    console.log(filteredRobots);
   }
 
 
   render() {
+    const filteredRobots = this.state.robots.filter(robots => {
+      return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+    })
+
     return (
       <div className='tc'>
         <h1>Robofriends</h1>
         <SearchBox searchChange={ this.onSearchChange } />
-        <CardList robots={ this.state.robots }/>
+        <CardList robots={ filteredRobots }/>
       </div>
     )
   }
