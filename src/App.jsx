5fb2ede9cfd3566472bda320386adfc31329fbbb
SCatella <samuel.catella@gmail.com>
2026-07-31 08:36:33 -0600

    feat(placeholder_api): add fetch block for user data

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import './App.css';
 import CardList from './CardList.jsx';
 import SearchBox from './SearchBox.jsx'
 import 'tachyons';
-import { robots } from './assets/robots.js';
 
 
 class App extends Component {
@@ -16,7 +15,9 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.setState({ robots: robots })
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }))
   }
 
 
@@ -30,13 +31,17 @@ class App extends Component {
       return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
     })
 
-    return (
-      <div className='tc'>
-        <h1 className='f1'>Robofriends</h1>
-        <SearchBox searchChange={ this.onSearchChange } />
-        <CardList robots={ filteredRobots }/>
-      </div>
-    )
+    if (this.state.robots.length === 0) {
+      return <h1>Loading...</h1>
+    } else {
+      return (
+        <div className='tc'>
+          <h1 className='f1'>Robofriends</h1>
+          <SearchBox searchChange={ this.onSearchChange } />
+          <CardList robots={ filteredRobots }/>
+        </div>
+      )
+    }
   }
 }
 
