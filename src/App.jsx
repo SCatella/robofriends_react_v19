83e08dbd496b2a6a1914611241eb28aa1593688d
SCatella <samuel.catella@gmail.com>
2026-07-30 21:32:32 -0600

    feat(search_box): add searchbox to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { Component } from 'react';
 import './App.css';
 import CardList from './CardList.jsx';
+import SearchBox from './SearchBox.jsx'
 import 'tachyons';
 import { robots } from './assets/robots.js';
 
 class App extends Component {
   render() {
     return (
-      <>
+      <div className='tc'>
         <h1>Robofriends</h1>
+        <SearchBox />
         <CardList robots={robots}/>
-      </>
+      </div>
     )
   }
 }
