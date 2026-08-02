29f056ae2a88467e7bd3a854f7b25cfa154b7b29
SCatella <samuel.catella@gmail.com>
2026-08-01 19:21:40 -0600

    fix(styling): fix responsive heading title for small screens

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -75,7 +75,7 @@ class App extends Component {
       <h1>Loading...</h1> :
       (
         <div className='tc'>
-          <h1 className='f1'>Robofriends</h1>
+          <h1 className='f2 f1-l'>Robofriends</h1>
           <SearchBox searchChange={ this.onSearchChange } />
           <Scroll>
             <CardList robots={ filteredRobots }/>
