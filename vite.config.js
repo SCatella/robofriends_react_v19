8fe755acfeeeff709cf0d50987d007f947166f8b
SCatella <samuel.catella@gmail.com>
2026-08-02 00:53:13 -0600

    ci(deploy): change url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/robofriends/',
+  base: '/robofriends_react_v19/',
   plugins: [react()],
 })
